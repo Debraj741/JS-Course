be1c270189966df2abf28db91f07b1ede32e0c3e
Debraj741 <128883544+Debraj741@users.noreply.github.com>
2023-09-25 08:35:20 +0000

    Datatype

---
+++ 01_Basics/datatype.js
@@ -0,0 +1,23 @@
+"use strict" //Treat all JS code as Newer version
+
+// alert("Hello")  //It is not use here as I am using Node Js note Browser.
+
+console.log(3+3)
+console.log("Debraj");
+
+let name="Debraj" 
+let age = 18
+let isgood = true
+
+/*
+Number
+string => " ";
+Bigint
+Boolean => True, false
+NULL - Standalone value (Empty representation)
+Undefined => No value assigned that indicate.
+Symbol => Unique
+*/
+
+console.log(typeof 15);
+
