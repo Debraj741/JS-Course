5ea87a506667a92630d9ddc8a350b66b6c54f611
Debraj741 <128883544+Debraj741@users.noreply.github.com>
2023-09-27 08:08:18 +0000

    Dates

---
+++ 01_Basics/dates.js
@@ -0,0 +1,25 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof mydate);
+
+// let newdate = new Date(2023,9,27)
+let newdate = new Date("09-27-2023")
+// console.log(newdate.toDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(newdate.getTime());
+
+
+// console.log(Date.now());
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+let date = new Date()
+console.log(date);
+console.log(date.getMinutes());
+console.log(date.getMonth());
