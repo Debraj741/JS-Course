18b40ce580c8575f9aa67b13afa88e9fd0ed29bf
Debraj741 <128883544+Debraj741@users.noreply.github.com>
2023-09-27 07:29:12 +0000

    Math & Number

---
+++ 01_Basics/math.js
@@ -0,0 +1,34 @@
+const balence = new Number(400)  //Specificly create Number
+// console.log(balence);
+
+
+// console.log(balence.toString());
+// console.log(balence.toFixed(2));
+
+const athornum = 85.369544
+// console.log(athornum.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/***********************************Maths*******************/
+
+// console.log(Math);
+// console.log(Math.abs(-78)); //Absolute value => abs
+// console.log(Math.round(7.69)); //Round value => round
+// console.log(Math.ceil(7.1));    //Upper value
+// console.log(Math.floor(7.9));   //Lower value
+
+// console.log(Math.min(7,8,2,9,1));   
+// console.log(Math.max(7,8,2,9,1));   
+
+// console.log(Math.random());    //Random value generated between 0-1
+// console.log((Math.random()*10)+1);
+// console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max-min+1))+min));
+console.log(Math.floor((Math.random() * (max-min+1))+min));
